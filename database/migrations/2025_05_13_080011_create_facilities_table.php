9c82d4648d5ca028e90877e148408044ce7626a2
severusDude <fajarsaladin05@gmail.com>
2025-05-13 15:36:50 +0700

    feat(facilities): add facility type

--- database/migrations/2025_05_13_080011_create_facilities_table.php
+++ database/migrations/2025_05_13_080011_create_facilities_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('facilities', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('name');
+            $table->string('type');
             $table->foreignUuid('default_image_id')->nullable()->constrained('attachments')->nullOnDelete();
             $table->timestamps();
         });
