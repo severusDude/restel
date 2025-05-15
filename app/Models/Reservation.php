037e8a14f67c60d4d4b17647dff29d7c7d340f91
Lutfi Fajar <60343449+severusDude@users.noreply.github.com>
2025-05-15 19:46:53 +0700

    feat (reservations): add reservation model

---
+++ app/Models/Reservation.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Reservation extends Model
+{
+    /** @use HasFactory<\Database\Factories\ReservationFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'start_date',
+        'end_date',
+        'status'
+    ];
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public static function booted(): void
+    {
+        static::creating(function ($model) {
+            $model->id = (string) Str::orderedUuid();
+        });
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             UserSeeder::class,
             FacilitySeeder::class,
-            RoomSeeder::class
+            RoomSeeder::class,
+            ReservationSeeder::class,
         ]);
     }
 }
